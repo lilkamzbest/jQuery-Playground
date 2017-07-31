a82f883b88c5a07a34951d21486706f505dc6d81
KAM <lilkamzbest1@gmail.com>
2017-07-31 09:44:20 +0100

    javascript code element even
    Try selecting all the even-numbered elements and giving them the classes of animated and shake.

---
+++ play.js
@@ -0,0 +1,14 @@
+<script>
+  $(document).ready(function() {
+    $("#target1").css("color", "red");
+    $("#target1").prop("disabled", true);
+    $("#target4").remove();
+    $("#target2").appendTo("#right-well");
+    $("#target5").clone().appendTo("#left-well");
+    $("#target1").parent().css("background-color", "red");
+    $("#right-well").children().css("color", "orange");
+    $("#left-well").children().css("color", "green");
+    $(".target:nth-child(2)").addClass("animated bounce");
+    $(".target:odd").addClass("animated shake");
+  });
+</script>
